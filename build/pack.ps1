509c3b587f2ba70cfd0ed14e8f7a500e0c102392
Bruno Brito <bhdebrito@gmail.com>
2019-11-01 02:28:36 -0300

    config

--- build/pack.ps1
+++ build/pack.ps1
@@ -1,4 +1,11 @@
+$version = $args[0]
+
+if (!$version)
+{
+    $version = '1.0.2-prerelease1.' + $([System.DateTime]::Now.ToString('MM-dd-HHmmss'))
+}
+Write-Host 'Version: ' $version 
 get-childitem * -include *.nupkg | remove-item
 dotnet build ..\src\JpProject.Core.sln
 dotnet test ..\src\JpProject.Core.sln
-dotnet pack ..\src\JpProject.Core.sln -o .\ -p:PackageVersion=1.0.1-$([System.DateTime]::Now.ToString('MM-dd-HHmmss'))
+dotnet pack ..\src\JpProject.Core.sln -o .\ -p:PackageVersion=$version
--- build/push.ps1
+++ build/push.ps1
@@ -1,4 +1,20 @@
+param([string] $source = "../.nuget",
+	  [switch] $prod)
+
+if($prod)
+{
+	$source = "https://api.nuget.org/v3/index.json"
+}
+
 $files = Get-ChildItem -recurse -filter *.nupkg
+
 foreach ($file in $files) {
-	nuget add $file.Name -source D:\workspace\local-nuget
+	if($prod)
+	{
+		dotnet nuget push $file.Name -s $source
+	}
+	else
+	{
+		nuget add $file.Name -source $source
+	}
 }
