0e29f8b7c719965762d0117acaa94099d222409d
Bruno <bhdebrito@gmail.com>
2020-04-11 04:03:42 -0300

    v3.2.3

--- tests/JPProject.Admin.Fakers.Test/ApiResourceFakers/ApiResourceFaker.cs
+++ tests/JPProject.Admin.Fakers.Test/ApiResourceFakers/ApiResourceFaker.cs
@@ -1,22 +1,24 @@
 ﻿using Bogus;
 using IdentityServer4.Models;
 using JPProject.Admin.Application.ViewModels.ApiResouceViewModels;
+using JPProject.Admin.Domain.Commands;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace JPProject.Admin.Fakers.Test.ApiResourceFakers
 {
     public class ApiResourceFaker
     {
-        public static Faker<ApiResource> GenerateApiResource()
+        public static Faker<ApiResource> GenerateApiResource(string name = null, bool addApiSecrets = true, bool addUserClaims = true)
         {
             return new Faker<ApiResource>()
-                .RuleFor(a => a.ApiSecrets, f => GenerateSecret().Generate(f.Random.Int(0, 2)))
+                .RuleFor(a => a.ApiSecrets, f => addApiSecrets ? GenerateSecret().Generate(f.Random.Int(0, 2)) : new List<Secret>())
                 .RuleFor(a => a.Enabled, f => f.Random.Bool())
-                .RuleFor(a => a.Name, f => f.Internet.DomainName())
+                .RuleFor(a => a.Name, f => name ?? f.Internet.DomainName())
                 .RuleFor(a => a.DisplayName, f => f.Lorem.Word())
-                .RuleFor(a => a.Scopes, f => GenerateScope().Generate(f.Random.Int(1,10)))
+                .RuleFor(a => a.Scopes, f => GenerateScope().Generate(f.Random.Int(1, 10)))
                 .RuleFor(a => a.Description, f => f.Lorem.Word())
-                .RuleFor(a => a.UserClaims, f => f.PickRandom(IdentityHelpers.Claims, f.Random.Int(0, 3)).ToList());
+                .RuleFor(a => a.UserClaims, f => addUserClaims ? f.PickRandom(IdentityHelpers.Claims, f.Random.Int(0, 3)).ToList() : new List<string>());
         }
 
         public static Faker<Secret> GenerateSecret()
@@ -38,13 +40,13 @@ namespace JPProject.Admin.Fakers.Test.ApiResourceFakers
                 .RuleFor(s => s.UserClaims, f => f.PickRandom(IdentityHelpers.Claims, f.Random.Int(0, 3)).ToList());
         }
 
-        public static Faker<SaveApiSecretViewModel> GenerateSaveClientSecret(string name)
+        public static Faker<SaveApiSecretViewModel> GenerateSaveClientSecret(string name, HashType? hash = null, string type = null)
         {
             return new Faker<SaveApiSecretViewModel>()
                 .RuleFor(s => s.Description, f => f.Lorem.Word())
                 .RuleFor(s => s.Value, f => f.Lorem.Word())
                 .RuleFor(s => s.Expiration, f => default)
-                .RuleFor(s => s.Hash, f => default)
+                .RuleFor(s => s.Hash, f => hash ?? f.PickRandom<HashType>())
                 .RuleFor(s => s.Type, f => f.Lorem.Word())
                 .RuleFor(s => s.ResourceName, f => name ?? f.Lorem.Word());
         }
