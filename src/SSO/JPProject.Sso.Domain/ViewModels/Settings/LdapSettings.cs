2f3644a1d4db62a804110e84d91a382084be8ccc
Bruno Brito <bhdebrito@gmail.com>
2020-07-06 10:21:57 -0300

    bug fixes (#19)
    * Recaptcha settings
    
    * removed install of net core 3.1
    
    * ldap and Syncronize claims
    
    * ldap more info
    
    * LDAP from Novel package
    
    * bug fix Api resource

--- src/SSO/JPProject.Sso.Domain/ViewModels/Settings/LdapSettings.cs
+++ src/SSO/JPProject.Sso.Domain/ViewModels/Settings/LdapSettings.cs
@@ -3,18 +3,30 @@
     public class LdapSettings
     {
         public LdapSettings() { }
-        public LdapSettings(string domainName, string distinguishedName, string attributes, string authType, string searchScope)
+        public LdapSettings(string domainName, string distinguishedName, string attributes, string authType, string searchScope, string portNumber, string fullyQualifiedDomainName, string connectionLess, string address)
         {
             DomainName = domainName;
             DistinguishedName = distinguishedName;
             Attributes = attributes;
             AuthType = authType;
             SearchScope = searchScope;
+            Address = address;
+            bool.TryParse(connectionLess, out var connectionParse);
+            ConnectionLess = connectionParse;
+            int.TryParse(portNumber, out var number);
+            PortNumber = number;
+
+            bool.TryParse(fullyQualifiedDomainName, out var fqdn);
+            FullyQualifiedDomainName = fqdn;
         }
         public string DomainName { get; set; }
         public string DistinguishedName { get; set; }
         public string Attributes { get; set; }
         public string AuthType { get; set; }
         public string SearchScope { get; set; }
+        public string Address { get; }
+        public bool FullyQualifiedDomainName { get; }
+        public bool ConnectionLess { get; }
+        public int PortNumber { get; set; }
     }
 }
